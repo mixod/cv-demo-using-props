8f17c1c8d6cc2e01dcc36c88b5b2d166959d739d
Sangram Thapa <thapasangram64@gmail.com>
2024-12-25 18:25:04 +0545

    practice of usestate

---
+++ CV/src/App.jsx
@@ -0,0 +1,60 @@
+// // import { useState } from "react"
+
+// import { useState } from "react"
+
+
+
+// function App() {
+//   const [state,setState]=useState(4);
+
+// function increment(){
+//   setState(state+1);
+// }
+// function decrement(){
+//   setState(state-1);
+// }
+
+//   return (
+//     <div>
+//       <button onClick={increment}>+</button>
+//       <spam>{state}</spam>
+//       <button onClick={decrement}>-</button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+// // function App() {
+// //   const [state, setState]=useState("RAM");
+
+
+// //   function change(){
+// //     setState("Hari");
+// //   }
+// //   return (
+// //     <>
+// //     <spam>{state}</spam>
+
+// //     <button onClick={change}>CLICK</button>
+// //     </>
+// //   )
+// // }
+
+// // export default App
+
+//Arrow function 
+ 
+function demo(){
+  
+}
+
+function App() {
+  return (
+    
+  )
+}
+
+export default App
